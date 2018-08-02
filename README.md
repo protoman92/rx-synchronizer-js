971c443e32998619664a56a328677be2ab9f4550
Hai Pham <swiften.svc@gmail.com>
2018-08-02 17:43:52 +0700

    chore(build): add package, build and test options

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # rx-synchronizer-js
+
 Commonly-used synchronizer bases that can be composed into more specific purposes.
---
+++ jest.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  roots: [
+    "<rootDir>",
+    "<rootDir>/src",
+  ],
+  "transform": {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  testRegex: "/test/.*.(test|spec)\\.tsx?$",
+  collectCoverage: true,
+  modulePaths: ['src'],
+  moduleFileExtensions: [
+    "ts",
+    "tsx",
+    "js",
+    "jsx",
+    "json",
+    "node"
+  ],
+  verbose: true,
+  testURL: 'http://localhost/',
+}
