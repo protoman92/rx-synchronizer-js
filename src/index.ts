f343a5f5f751a93e566448cb81ef74942dc4f39c
Hai Pham <swiften.svc@gmail.com>
2018-08-02 22:57:16 +0700

    feat(synchronizer): export all synchronizers

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import * as CleanUpSync from './cleanup';
+import * as FetchSync from './fetch';
+import * as ModifySync from './modify';
+import * as ValidateSync from './validate';
+export { CleanUpSync, FetchSync, ModifySync, ValidateSync };
