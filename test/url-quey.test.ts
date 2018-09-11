2cdeccffc618916857ead88119761eb451cfd730
Hai Pham <swiften.svc@gmail.com>
2018-09-11 11:48:47 +0800

    feat(test): add async handling for url query test

--- test/url-quey.test.ts
+++ test/url-quey.test.ts
@@ -12,6 +12,7 @@ import {
   when,
 } from 'ts-mockito-2';
 import * as UrlQuerySync from 'url-query';
+import {asyncTimeout, asyncWait} from './test-util';
 
 describe('Url query sync should work correctly', () => {
   type Query = Readonly<{a: string; b: string}>;
@@ -35,38 +36,45 @@ describe('Url query sync should work correctly', () => {
     urlQuerySync = new UrlQuerySync.Impl(instance(triggerSync));
   });
 
-  it('Invoking synchronization - should map to base dependency', () => {
-    /// Setup
-    let acceptableUrlPath = 'acceptable-url';
-    let queryStream = new Subject<Query>();
-    let urlPathStream = new BehaviorSubject<Undefined<string>>(undefined);
-    when(dependency.acceptableUrlPathName).thenReturn(acceptableUrlPath);
-    when(dependency.queryStream).thenReturn(queryStream);
-    when(dependency.urlPathNameStream).thenReturn(urlPathStream);
+  it(
+    'Invoking synchronization - should map to base dependency',
+    done => {
+      /// Setup
+      let acceptableUrlPath = 'acceptable-url';
+      let queryStream = new Subject<Query>();
+      let urlPathStream = new BehaviorSubject<Undefined<string>>(undefined);
+      when(dependency.acceptableUrlPathName).thenReturn(acceptableUrlPath);
+      when(dependency.queryStream).thenReturn(queryStream);
+      when(dependency.urlPathNameStream).thenReturn(urlPathStream);
 
-    urlQuerySync.synchronize(instance(dependency));
-    let mappedDepn = capture(
-      triggerSync.synchronize
-    ).first()[0] as TriggerSync.Depn<Query>;
+      urlQuerySync.synchronize(instance(dependency));
+      let mappedDepn = capture(
+        triggerSync.synchronize
+      ).first()[0] as TriggerSync.Depn<Query>;
 
-    mappedDepn.triggerStream.subscribe({...instance(urlQueryReceiver)});
+      mappedDepn.triggerStream.subscribe({...instance(urlQueryReceiver)});
 
-    /// When
-    urlPathStream.next(acceptableUrlPath);
-    queryStream.next({a: '1', b: '2'});
-    queryStream.next({a: '1', b: '2'});
-    queryStream.next({a: '2', b: '3'});
-    urlPathStream.next('invalid-url');
-    queryStream.next({a: '2', b: '3'});
-    queryStream.next({a: '3', b: '4'});
-    urlPathStream.next(acceptableUrlPath);
+      /// When
+      urlPathStream.next(acceptableUrlPath);
+      queryStream.next({a: '1', b: '2'});
+      queryStream.next({a: '1', b: '2'});
+      queryStream.next({a: '2', b: '3'});
+      urlPathStream.next('invalid-url');
+      queryStream.next({a: '2', b: '3'});
+      queryStream.next({a: '3', b: '4'});
+      urlPathStream.next(acceptableUrlPath);
 
-    /// Then
-    verify(urlQueryReceiver.next(anything())).times(2);
-    verify(urlQueryReceiver.next(deepEqual({a: '1', b: '2'}))).once();
-    verify(urlQueryReceiver.next(deepEqual({a: '2', b: '3'}))).once();
-    verify(urlQueryReceiver.next(deepEqual({a: '3', b: '4'}))).never();
-  });
+      setTimeout(() => {
+        /// Then
+        verify(urlQueryReceiver.next(anything())).times(2);
+        verify(urlQueryReceiver.next(deepEqual({a: '1', b: '2'}))).once();
+        verify(urlQueryReceiver.next(deepEqual({a: '2', b: '3'}))).once();
+        verify(urlQueryReceiver.next(deepEqual({a: '3', b: '4'}))).never();
+        done();
+      }, asyncWait);
+    },
+    asyncTimeout
+  );
 
   it('Constructing synchronizer with default arguments - should work', () => {
     let urlQuerySync2 = new UrlQuerySync.Impl();
