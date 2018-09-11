96a4982a0e5ff813b1a8485ab1f158e6e7311590
Hai Pham <swiften.svc@gmail.com>
2018-09-11 11:25:53 +0800

    feat(test): add async handling for find-index test

--- test/find-index.test.ts
+++ test/find-index.test.ts
@@ -11,6 +11,7 @@ import {
   verify,
   when,
 } from 'ts-mockito-2';
+import {asyncTimeout, asyncWait} from './test-util';
 
 describe('Find index synchronizer should work correctly', () => {
   type Indexed = {readonly id?: string; readonly name?: string};
@@ -35,86 +36,107 @@ describe('Find index synchronizer should work correctly', () => {
     findIndexSync = new FindIndexSync(instance(triggerSync));
   });
 
-  it('Invoking synchronization - should map dependency to base dependency', () => {
-    /// Setup
-    let objectStream = new Subject<Try<Indexed[]>>();
-    let objectPropStream = new Subject<Try<string>>();
-    when(dependency.allowInvalidResult).thenReturn(true);
-    when(dependency.allObjectStream).thenReturn(objectStream);
-    when(dependency.objectPropStream).thenReturn(objectPropStream);
-    findIndexSync.synchronize(instance(dependency));
-    let mappedDepn = capture(triggerSync.synchronize).first()[0] as TriggerDepn<
-      Never<number>
-    >;
-    mappedDepn.triggerStream.subscribe({...instance(indexReceiver)});
+  it(
+    'Invoking synchronization - should map dependency to base dependency',
+    done => {
+      /// Setup
+      let objectStream = new Subject<Try<Indexed[]>>();
+      let objectPropStream = new Subject<Try<string>>();
+      when(dependency.allowInvalidResult).thenReturn(true);
+      when(dependency.allObjectStream).thenReturn(objectStream);
+      when(dependency.objectPropStream).thenReturn(objectPropStream);
+      findIndexSync.synchronize(instance(dependency));
+      let mappedDepn = capture(
+        triggerSync.synchronize
+      ).first()[0] as TriggerDepn<Never<number>>;
+      mappedDepn.triggerStream.subscribe({...instance(indexReceiver)});
 
-    /// When
-    objectStream.next(Try.failure(''));
-    objectPropStream.next(Try.failure(''));
-    objectStream.next(Try.success([{}, {}, {id: '1', name: '2'}]));
-    objectPropStream.next(Try.failure(''));
-    objectPropStream.next(Try.success('1'));
-    objectPropStream.next(Try.failure(''));
-    objectPropStream.next(Try.success('2'));
-    objectStream.next(Try.success([{}, {}, {}]));
+      /// When
+      objectStream.next(Try.failure(''));
+      objectPropStream.next(Try.failure(''));
+      objectStream.next(Try.success([{}, {}, {id: '1', name: '2'}]));
+      objectPropStream.next(Try.failure(''));
+      objectPropStream.next(Try.success('1'));
+      objectPropStream.next(Try.failure(''));
+      objectPropStream.next(Try.success('2'));
+      objectStream.next(Try.success([{}, {}, {}]));
 
-    /// Then
-    verify(indexReceiver.next(undefined)).thrice();
-    verify(indexReceiver.next(2)).twice();
-    verify(indexReceiver.next(anything())).times(5);
-  });
+      setTimeout(() => {
+        /// Then
+        verify(indexReceiver.next(undefined)).thrice();
+        verify(indexReceiver.next(2)).twice();
+        verify(indexReceiver.next(anything())).times(5);
+        done();
+      }, asyncWait);
+    },
+    asyncTimeout
+  );
 
-  it('Disallowing invalid results - should filter out invalid results', () => {
-    /// Setup
-    let objectStream = new Subject<Try<Indexed[]>>();
-    let objectPropStream = new Subject<Try<string>>();
-    when(dependency.allowInvalidResult).thenReturn(false);
-    when(dependency.allObjectStream).thenReturn(objectStream);
-    when(dependency.objectPropStream).thenReturn(objectPropStream);
-    findIndexSync.synchronize(instance(dependency));
-    let mappedDepn = capture(triggerSync.synchronize).first()[0] as TriggerDepn<
-      Never<number>
-    >;
-    mappedDepn.triggerStream.subscribe({...instance(indexReceiver)});
+  it(
+    'Disallowing invalid results - should filter out invalid results',
+    done => {
+      /// Setup
+      let objectStream = new Subject<Try<Indexed[]>>();
+      let objectPropStream = new Subject<Try<string>>();
+      when(dependency.allowInvalidResult).thenReturn(false);
+      when(dependency.allObjectStream).thenReturn(objectStream);
+      when(dependency.objectPropStream).thenReturn(objectPropStream);
+      findIndexSync.synchronize(instance(dependency));
+      let mappedDepn = capture(
+        triggerSync.synchronize
+      ).first()[0] as TriggerDepn<Never<number>>;
+      mappedDepn.triggerStream.subscribe({...instance(indexReceiver)});
 
-    /// When
-    objectStream.next(Try.failure(''));
-    objectPropStream.next(Try.failure(''));
-    objectStream.next(Try.success([{}, {}, {id: '1'}]));
-    objectPropStream.next(Try.failure(''));
-    objectPropStream.next(Try.success('1'));
-    objectStream.next(Try.success([{}, {id: '1', name: '2'}, {}]));
-    objectPropStream.next(Try.success('2'));
-    objectStream.next(Try.success([{}, {}, {}]));
+      /// When
+      objectStream.next(Try.failure(''));
+      objectPropStream.next(Try.failure(''));
+      objectStream.next(Try.success([{}, {}, {id: '1'}]));
+      objectPropStream.next(Try.failure(''));
+      objectPropStream.next(Try.success('1'));
+      objectStream.next(Try.success([{}, {id: '1', name: '2'}, {}]));
+      objectPropStream.next(Try.success('2'));
+      objectStream.next(Try.success([{}, {}, {}]));
 
-    /// Then
-    verify(indexReceiver.next(1)).once();
-    verify(indexReceiver.next(2)).once();
-    verify(indexReceiver.next(anything())).twice();
-  });
+      setTimeout(() => {
+        /// Then
+        verify(indexReceiver.next(1)).once();
+        verify(indexReceiver.next(2)).once();
+        verify(indexReceiver.next(anything())).twice();
+        done();
+      }, asyncWait);
+    },
+    asyncTimeout
+  );
 
-  it('Passing one object key - should be converted into key array', () => {
-    /// Setup
-    let objectStream = new Subject<Try<Indexed[]>>();
-    let objectPropStream = new Subject<Try<string>>();
-    when(dependency.allowInvalidResult).thenReturn(false);
-    when(dependency.allObjectStream).thenReturn(objectStream);
-    when(dependency.objectPropStream).thenReturn(objectPropStream);
-    when(dependency.objectPropKeys).thenReturn('id');
-    findIndexSync.synchronize(instance(dependency));
-    let mappedDepn = capture(triggerSync.synchronize).first()[0] as TriggerDepn<
-      Never<number>
-    >;
-    mappedDepn.triggerStream.subscribe({...instance(indexReceiver)});
+  it(
+    'Passing one object key - should be converted into key array',
+    done => {
+      /// Setup
+      let objectStream = new Subject<Try<Indexed[]>>();
+      let objectPropStream = new Subject<Try<string>>();
+      when(dependency.allowInvalidResult).thenReturn(false);
+      when(dependency.allObjectStream).thenReturn(objectStream);
+      when(dependency.objectPropStream).thenReturn(objectPropStream);
+      when(dependency.objectPropKeys).thenReturn('id');
+      findIndexSync.synchronize(instance(dependency));
+      let mappedDepn = capture(
+        triggerSync.synchronize
+      ).first()[0] as TriggerDepn<Never<number>>;
+      mappedDepn.triggerStream.subscribe({...instance(indexReceiver)});
 
-    /// When
-    objectStream.next(Try.success([{}, {}, {id: '1'}]));
-    objectPropStream.next(Try.success('1'));
-    objectPropStream.next(Try.success('2'));
-    objectStream.next(Try.success([{}, {id: '1', name: '2'}, {}]));
+      /// When
+      objectStream.next(Try.success([{}, {}, {id: '1'}]));
+      objectPropStream.next(Try.success('1'));
+      objectPropStream.next(Try.success('2'));
+      objectStream.next(Try.success([{}, {id: '1', name: '2'}, {}]));
 
-    /// Then
-    verify(indexReceiver.next(2)).once();
-    verify(indexReceiver.next(anything())).once();
-  });
+      setTimeout(() => {
+        /// Then
+        verify(indexReceiver.next(2)).once();
+        verify(indexReceiver.next(anything())).once();
+        done();
+      }, asyncWait);
+    },
+    asyncTimeout
+  );
 });
