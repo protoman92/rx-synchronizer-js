a51667c8cc03d5e5a608db12cc652586ba376061
Hai Pham <swiften.svc@gmail.com>
2018-09-11 11:52:23 +0800

    feat(test): add async handling for validate test

--- test/validate.test.ts
+++ test/validate.test.ts
@@ -2,6 +2,7 @@ import {IGNORE, Ignore, Never, Numbers, Try} from 'javascriptutilities';
 import {NEVER, NextObserver, Subject} from 'rxjs';
 import {anything, capture, instance, spy, verify, when} from 'ts-mockito-2';
 import {Depn as ValidateDepn, Impl as ValidateSync} from 'validate';
+import {asyncTimeout, asyncWait} from './test-util';
 
 describe('Validate sync should work correctly', () => {
   let dependency: ValidateDepn<number>;
@@ -21,89 +22,124 @@ describe('Validate sync should work correctly', () => {
     synchronizer = new ValidateSync();
   });
 
-  it('Streaming duplicate params - should receive only unique params', () => {
-    /// Setup
-    let objectStream = new Subject<Try<number>>();
-    when(dependency.objectStream).thenReturn(objectStream);
-    synchronizer.synchronize(instance(dependency));
+  it(
+    'Streaming duplicate params - should receive only unique params',
+    done => {
+      /// Setup
+      let objectStream = new Subject<Try<number>>();
+      when(dependency.objectStream).thenReturn(objectStream);
+      synchronizer.synchronize(instance(dependency));
 
-    /// When
-    Numbers.range(0, 1000).forEach(() => objectStream.next(Try.success(0)));
+      /// When
+      Numbers.range(0, 1000).forEach(() => objectStream.next(Try.success(0)));
 
-    /// Then
-    verify(dependency.validateObject(anything())).once();
-  });
+      setTimeout(() => {
+        /// Then
+        verify(dependency.validateObject(anything())).once();
+        done();
+      }, asyncWait);
+    },
+    asyncTimeout
+  );
 
-  it('Validating fails - should broadcast error', () => {
-    /// Setup
-    let error = 'Error!!!';
-    let objectStream = new Subject<Try<number>>();
-    when(dependency.objectStream).thenReturn(objectStream);
-    when(dependency.validateObject(anything())).thenCall(() => {
-      throw new Error(error);
-    });
+  it(
+    'Validating fails - should broadcast error',
+    done => {
+      /// Setup
+      let error = 'Error!!!';
+      let objectStream = new Subject<Try<number>>();
+      when(dependency.objectStream).thenReturn(objectStream);
+      when(dependency.validateObject(anything())).thenCall(() => {
+        throw new Error(error);
+      });
 
-    synchronizer.synchronize(instance(dependency));
+      synchronizer.synchronize(instance(dependency));
 
-    /// When
-    objectStream.next(Try.success(0));
+      /// When
+      objectStream.next(Try.success(0));
 
-    /// Then
-    verify(dependency.validateObject(anything())).once();
-    expect(capture(errorReceiver.next).first()[0]!.message).toEqual(error);
-  });
+      setTimeout(() => {
+        /// Then
+        verify(dependency.validateObject(anything())).once();
+        expect(capture(errorReceiver.next).first()[0]!.message).toEqual(error);
+        done();
+      }, asyncWait);
+    },
+    asyncTimeout
+  );
 
-  it('Validating fails with same error - should only broadcast uniques', () => {
-    /// Setup
-    let times = 1000;
-    let error = 'Error!!!';
-    let objectStream = new Subject<Try<number>>();
-    when(dependency.objectStream).thenReturn(objectStream);
-    when(dependency.validateObject(anything())).thenCall(() => {
-      throw new Error(error);
-    });
+  it(
+    'Validating fails with same error - should only broadcast uniques',
+    done => {
+      /// Setup
+      let times = 1000;
+      let error = 'Error!!!';
+      let objectStream = new Subject<Try<number>>();
+      when(dependency.objectStream).thenReturn(objectStream);
+      when(dependency.validateObject(anything())).thenCall(() => {
+        throw new Error(error);
+      });
 
-    synchronizer.synchronize(instance(dependency));
+      synchronizer.synchronize(instance(dependency));
 
-    /// When
-    Numbers.range(0, times).forEach(v => objectStream.next(Try.success(v)));
+      /// When
+      Numbers.range(0, times).forEach(v => objectStream.next(Try.success(v)));
 
-    /// Then
-    verify(dependency.validateObject(anything())).times(times);
-    verify(errorReceiver.next(anything())).once();
-    expect(capture(errorReceiver.next).first()[0]!.message).toEqual(error);
-  });
+      setTimeout(() => {
+        /// Then
+        verify(dependency.validateObject(anything())).times(times);
+        verify(errorReceiver.next(anything())).once();
+        expect(capture(errorReceiver.next).first()[0]!.message).toEqual(error);
+        done();
+      }, asyncWait);
+    },
+    asyncTimeout
+  );
 
-  it('Validating succeeds - should only broadcast uniques', () => {
-    /// Setup
-    let times = 1000;
-    let objectStream = new Subject<Try<number>>();
-    when(dependency.objectStream).thenReturn(objectStream);
-    when(dependency.validateObject(anything())).thenCall(() => {});
-    synchronizer.synchronize(instance(dependency));
+  it(
+    'Validating succeeds - should only broadcast uniques',
+    done => {
+      /// Setup
+      let times = 1000;
+      let objectStream = new Subject<Try<number>>();
+      when(dependency.objectStream).thenReturn(objectStream);
+      when(dependency.validateObject(anything())).thenCall(() => {});
+      synchronizer.synchronize(instance(dependency));
 
-    /// When
-    Numbers.range(0, times).forEach(v => objectStream.next(Try.success(v)));
+      /// When
+      Numbers.range(0, times).forEach(v => objectStream.next(Try.success(v)));
 
-    /// Then
-    verify(dependency.validateObject(anything())).times(times);
-    verify(errorReceiver.next(undefined)).once();
-  });
+      setTimeout(() => {
+        /// Then
+        verify(dependency.validateObject(anything())).times(times);
+        verify(errorReceiver.next(undefined)).once();
+        done();
+      }, asyncWait);
+    },
+    asyncTimeout
+  );
 
-  it('Sending stop signal - should unsubscribe all streams', () => {
-    /// Setup
-    let objectStream = new Subject<Try<number>>();
-    let stopStream = new Subject<Ignore>();
-    when(dependency.objectStream).thenReturn(objectStream);
-    when(dependency.stopStream).thenReturn(stopStream);
-    synchronizer.synchronize(instance(dependency));
+  it(
+    'Sending stop signal - should unsubscribe all streams',
+    done => {
+      /// Setup
+      let objectStream = new Subject<Try<number>>();
+      let stopStream = new Subject<Ignore>();
+      when(dependency.objectStream).thenReturn(objectStream);
+      when(dependency.stopStream).thenReturn(stopStream);
+      synchronizer.synchronize(instance(dependency));
 
-    /// When
-    objectStream.next(Try.success(0));
-    stopStream.next(IGNORE);
-    Numbers.range(0, 1000).forEach(v => objectStream.next(Try.success(v)));
+      /// When
+      objectStream.next(Try.success(0));
+      stopStream.next(IGNORE);
+      Numbers.range(0, 1000).forEach(v => objectStream.next(Try.success(v)));
 
-    /// Then
-    verify(errorReceiver.next(anything())).once();
-  });
+      setTimeout(() => {
+        /// Then
+        verify(errorReceiver.next(anything())).once();
+        done();
+      }, asyncWait);
+    },
+    asyncTimeout
+  );
 });
